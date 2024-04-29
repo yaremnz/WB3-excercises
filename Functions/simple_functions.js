64e35bcea639920a03c936767330bb008d77865f
Yareli Cerda <yarelic.1002@gmail.com>
2024-04-29 12:50:39 -0400

    excercise 1

---
+++ Functions/simple_functions.js
@@ -0,0 +1,29 @@
+//inputs
+
+
+function favoriteThings(){
+    let name = "Yareli";
+    let favoriteMovie = "Shrek2";
+    let favoriteBand = "Coldplay";
+
+    console.log("Name: " + name);
+    console.log("Movie: " + favoriteMovie);
+    console.log("Favorite Band: " + favoriteBand);
+}
+
+function whyImHere(){
+    console.log("To become a proficient web developer.")
+}
+
+function favoritePlace(){
+    console.log("My favorite place to visit is Tennese.")
+}
+
+
+
+
+
+
+favoriteThings();
+whyImHere();
+favoritePlace();
