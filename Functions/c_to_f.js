894117f97e921db5cc6ae58112a749a3bd5aabe3
Yareli Cerda <yarelic.1002@gmail.com>
2024-04-29 15:34:46 -0400

    c to f

---
+++ Functions/c_to_f.js
@@ -0,0 +1,14 @@
+
+function convertCtoF(c){
+    let farenheitTemp = (c * (9/5) + 32)
+    return farenheitTemp
+}
+
+console.log(convertCtoF(100));
+console.log(convertCtoF(45));
+console.log(convertCtoF(19));
+console.log(convertCtoF(0));
+console.log(convertCtoF(-7));
+console.log(convertCtoF(-40));
+
+
